58b8800adbea451981d4742fc826968bc14099a7
Chalani Rajapaksha <chalani.rajapaksha@elegantmedia.lk>
2023-02-18 16:20:17 +0530

    sign in and sign up done and connected firebase

---
+++ app/src/main/java/com/example/pet_care/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.codingstuff.loginandsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pet_care.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
